ccc8a31b6806f4aeba0e0ebdb3f3d42fc72c3542
svivek19 <svivek030503@gmail.com>
2025-12-24 12:04:41 +0530

    feat: add getAllUsers API to retrieve a list of users with name, email, and role

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -49,3 +49,12 @@ export const loginAdmin = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("name email role");
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { registerAdmin, loginAdmin } from "../controllers/auth.controller.js";
+import {
+  registerAdmin,
+  loginAdmin,
+  getAllUsers,
+} from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", registerAdmin);
 authRouter.post("/login", loginAdmin);
+authRouter.get("/users", getAllUsers);
 
 export default authRouter;
